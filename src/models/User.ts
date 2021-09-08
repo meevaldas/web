7a6f49a39a3ad8e4b6e09ab13e256084b632d21a
Meeval Das <meevadas13@gmail.com>
2021-09-08 21:45:04 +0530

    TSC file

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,8 @@
 //the properties in a vertical manner. Also it creates a confusion
 interface UserProps{
     name:string,
+    //name?: string - Means , this is optional. A user can have a name
+    //but not necessary
     age: number
 }
 
