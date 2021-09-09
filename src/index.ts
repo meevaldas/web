88611563f742363cf845d4fb52016c6911886dd1
Meeval Das <meevadas13@gmail.com>
2021-09-09 17:22:12 +0530

    Added more methods

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import {User} from './models/User';
 const user = new User({name:'Brendon',age:5});
 user.set({name: 'Daniela', age:25});
 
-user.get('name');
-user.get('age');
+user.on('change',() => {});
+
+console.log(user);
+/*user.get('name');
+user.get('age');*/
 //changes
--- src/models/User.ts
+++ src/models/User.ts
@@ -7,16 +7,38 @@ interface UserProps{
     age: number
 }
 
+type Callback = () => void;
+
 export class User {
+    events: {[key:string]: Callback} = {}
+
     constructor(private data: UserProps) {
     }
 
-    get(propName: string): string | number {
+    get(propName: keyof UserProps): string | number {
         return this.data[propName];
     }
 
     set(update: UserProps): void {
         Object.assign(this.data, update);
         //here comes the error
+    }
+
+    on(eventName:string, callback: Callback):void{
+        const handlers = this.events[eventName] || [];
+        handlers.push(callback);
+        this.events[eventName] = handlers;
+    }
+
+    trigger(eventName: string): void{
+        const handlers = this.events[eventName];
+
+        if(!handlers || handlers.length === 0){
+            return;
+        }
+
+        handlers.foreach(callback => {
+            callback();
+        });
     }
 }
